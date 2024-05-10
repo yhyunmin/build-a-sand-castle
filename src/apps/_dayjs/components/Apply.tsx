526d38cab9595073817f6a7e70ac66e31791fb1f
yhyunmin <smurf@kakao.com>
2024-05-10 09:25:40 +0900

    add : dayjs apply

--- src/apps/_dayjs/components/Apply.tsx
+++ src/apps/_dayjs/components/Apply.tsx
@@ -5,12 +5,21 @@ dayjs.extend(relativeTime);
 dayjs.locale('ko');
 
 //todo 렌더링 기점 createAt 만들고 clone 하기
+const createdAt = '2023-11-02T23:56:00+09:00';
+const now = dayjs(new Date());
+const updatedAt = dayjs(now).clone();
+const expiredAt = dayjs('2024-05-21');
+// const createdAt = clone.toNow();
 const product = {
   title: 'dummy',
   amount: '1',
   price: '1000',
-  createdAt: new Date(),
+  createdAt,
+  updatedAt,
 };
+const diffDays = expiredAt.diff(createdAt, 'day');
+const month = Math.floor(diffDays / 30);
+const days = diffDays % 30;
 
 const Apply = () => {
   return (
@@ -33,10 +42,13 @@ const Apply = () => {
         프로덕트 가격 <span>{product.price}</span>
       </p>
       <p>
-        프로덕트 시간{' '}
-        <span>
-          {dayjs(product.createdAt).format('YYYY년.MMMM.DD일 HH시mm분')}
-        </span>
+        프로덕트 최초등록 <span>{dayjs(product.createdAt).fromNow()}</span>
+      </p>
+      <p>
+        프로덕트 업데이트 <span>{dayjs(product.updatedAt).fromNow()}</span>
+      </p>
+      <p>
+        프로덕트 만료기간 <span>{`${month}개월 ${days}일 남음`}</span>
       </p>
     </>
   );
