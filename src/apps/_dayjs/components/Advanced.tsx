19e20606b452d25bb1d542aab841bd71eee9e120
yhyunmin <smurf@kakao.com>
2024-05-10 15:24:21 +0900

    fixed rendering issue caused useState, add to use useEffect

--- src/apps/_dayjs/components/Advanced.tsx
+++ src/apps/_dayjs/components/Advanced.tsx
@@ -1,29 +1,86 @@
 import Calendar from './Calendar.tsx';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
 
 const Advanced = () => {
-  const currentDate = dayjs(new Date());
+  const [date, setDate] = useState(dayjs());
+  const [firstDayOfWeek, setFirstDayOfWeek] = useState(0);
+  const [isVisible, setIsVisible] = useState(false);
+  const [emptyCell, setEmptyCell] = useState<Array<null>>([]);
+  const [cell, setCell] = useState<Array<number>>([]);
 
-  const [year, setYear] = useState(currentDate.year());
-  const [month, setMonth] = useState(currentDate.month() + 1);
-  const [firstDayofWeek, setFirstDayofWeek] = useState(
-    dayjs().date(1).get('day'),
-  );
-  const emptyCell: null[] = new Array(firstDayofWeek).fill(null);
-  const cell = new Array(dayjs(`${year}-${month}`).daysInMonth())
-    .fill(null)
-    .map((_, i) => i + 1);
+  const handleNextMonth = () => {
+    setDate((prev) => dayjs(prev).add(1, 'M'));
+  };
+
+  const handlePrevMonth = () => {
+    setDate((prev) => dayjs(prev).subtract(1, 'M'));
+  };
+
+  const handleSetCustomDate = (year, month) => {};
+
+  const handleVisible = () => {
+    setIsVisible(!isVisible);
+  };
+
+  const onInit = () => {
+    const currentDate = dayjs();
+    setDate(currentDate);
+    setFirstDayOfWeek(currentDate.date(1).get('day'));
+    setEmptyCell(Array(currentDate.date(1).get('day')).fill(null));
+    setCell(
+      Array(currentDate.daysInMonth())
+        .fill(null)
+        .map((_, i) => i + 1),
+    );
+  };
+
+  useEffect(() => {
+    onInit();
+    console.log('init');
+  }, []);
+
+  useEffect(() => {
+    const handleCells = (firstDayofweek: number) => {
+      setEmptyCell(Array(firstDayofweek).fill(null));
+      setCell(
+        Array(dayjs(date).daysInMonth())
+          .fill(null)
+          .map((_, i) => i + 1),
+      );
+    };
+    setFirstDayOfWeek(dayjs(date).date(1).get('day'));
+    handleCells(firstDayOfWeek);
+  }, [firstDayOfWeek, date]);
 
   return (
     <>
       <h2>Advanced</h2>
-      <p>year {year}</p>
-      <p>month {month}</p>
-      <p>firstDayofWeek {firstDayofWeek}</p>
+      <p>year {dayjs(date).year()}</p>
+      <p>month {dayjs(date).month()}</p>
+      <p>firstDayofWeek {firstDayOfWeek}</p>
       <p>emptyCell 갯수 {emptyCell.length}</p>
       <hr />
-      <Calendar emptyCell={emptyCell} cell={cell} />
+      {isVisible && (
+        <>
+          년
+          <input type="text" />
+          월
+          <input type="text" />
+          <button>확인</button>
+        </>
+      )}
+      <Calendar
+        year={dayjs(date).format('YYYY')}
+        month={dayjs(date).format('MM')}
+        emptyCell={emptyCell}
+        cell={cell}
+        handleNextMonth={handleNextMonth}
+        handlePrevMonth={handlePrevMonth}
+        handleSetCustomDate={handleSetCustomDate}
+        isVisible={isVisible}
+        handleVisible={handleVisible}
+      />
     </>
   );
 };
--- src/apps/_dayjs/components/Calendar.tsx
+++ src/apps/_dayjs/components/Calendar.tsx
@@ -1,30 +1,48 @@
 import style from '../style/calendar.module.css';
 
 type Props = {
+  year: string;
+  month: string;
   emptyCell: null[];
   cell: number[];
+  handleSetCustomDate: (year: number, month: number) => void;
+  handlePrevMonth: (month: number) => void;
+  handleNextMonth: (month: number) => void;
+  handleVisible: () => void;
+  isVisible: boolean;
 };
-const Calendar = ({ emptyCell, cell }: Props) => {
+const Calendar = ({
+  year,
+  month,
+  emptyCell,
+  cell,
+  handleSetCustomDate,
+  handleVisible,
+  handlePrevMonth,
+  handleNextMonth,
+}: Props) => {
   return (
     <>
       <h2>Calendar</h2>
       <div className={style.container}>
         <div className={style.header}>
-          <h3>2024 May</h3>
+          <h3 onClick={() => handleVisible()}>
+            {year}년 {month}월
+          </h3>
           <div className={style.btnGroup}>
-            <button>{'<'}</button>
-            <button>{'>'}</button>
+            <button onClick={() => handlePrevMonth(1)}>{'<'}</button>
+            <button onClick={() => handleNextMonth(1)}>{'>'}</button>
           </div>
         </div>
         <div className={style.body}>
           <div className={style.days}>
+            <div className={style.daysCol}>Sun</div>
             <div className={style.daysCol}>Mon</div>
             <div className={style.daysCol}>Tue</div>
             <div className={style.daysCol}>Wed</div>
             <div className={style.daysCol}>Thu</div>
             <div className={style.daysCol}>Fri</div>
             <div className={style.daysCol}>Sat</div>
-            <div className={style.daysCol}>Sun</div>
           </div>
           <div className={style.dates}>
             {emptyCell.map(() => {
