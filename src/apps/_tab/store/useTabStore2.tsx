05a0b59dabb525a4ff2a95ea7f4605142d092654
smurf <smurf@kakao.com>
2024-07-03 18:02:28 +0900

    adding useTabstore 2

--- src/apps/_tab/store/useTabStore2.tsx
+++ src/apps/_tab/store/useTabStore2.tsx
@@ -1,4 +1,13 @@
 import { create } from 'zustand';
 
 type TODO = any;
-const useTabStore2 = create<TODO>((set, get) => {});
+const useTabStore2 = create<TODO>((set, get) => {
+  return {
+    formBuilderStore: {
+      id: 0, // index
+      type: 'input', // select,input
+      label: 'input-title', // label
+      contents: ['value'], // input : contents[0], select : contents[1,2,3...]
+    },
+  };
+});
