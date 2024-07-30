458c055e02991abff3207c144072d19710ae54e8
DESKTOP-EAEUNL5\withusint <jhwithhm@gmail.com>
2024-07-30 09:28:17 +0900

    add : init folder tree

--- src/apps/_recursive/recursive.tsx
+++ src/apps/_recursive/recursive.tsx
@@ -1,18 +1,39 @@
 import { FolderIcon } from 'lucide-react';
 
-export const Recursive = () =>{
+export const Recursive = () => {
+  const folders = ['Movies', 'Music', 'Pictures'];
   return (
-  <>
-    <div className="p-8 max-w-sm mx-auto">
-      <ul>
-        <li className="my-1.5">
-          <span className="flex items-center gap-1.5">
-          <FolderIcon className="size-6 text-sky-500"/>
-            Home
-          </span>
-        </li>
-      </ul>
-    </div>
-  </>
-  )
-}
+    <>
+      <div className="p-8 max-w-sm mx-auto">
+        <ul>
+          <li className="my-1.5">
+            <span className="flex items-center gap-1.5">
+              <FolderIcon className="size-6 text-sky-500" />
+              Home
+            </span>
+            <ul className="pl-6">
+              <li className="my-1.5">
+                <span className="flex items-center gap-1.5">
+                  <FolderIcon className="size-6 text-sky-500" />
+                  Movies
+                </span>
+              </li>
+              <li className="my-1.5">
+                <span className="flex items-center gap-1.5">
+                  <FolderIcon className="size-6 text-sky-500" />
+                  Music
+                </span>
+              </li>
+              <li className="my-1.5">
+                <span className="flex items-center gap-1.5">
+                  <FolderIcon className="size-6 text-sky-500" />
+                  Pictures
+                </span>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
